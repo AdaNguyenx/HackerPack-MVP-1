92118ce7aa16e9f1f5e1cf9fffb8429b13fb3963
Jaime5 <Jaime5@UMBC.edu>
2016-06-14 12:20:47 -0400

    Add team modularization page

--- src/urls.py
+++ src/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^company/$', views.company, name='company')
+	url(r'^company/$', views.company, name='company'),
+	# Add refs for:
+	# - user login
+	# - tutorials
 ]
